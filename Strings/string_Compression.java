7fabc9e162d0cdb661ff2d7cf2ecea7242b0e4e8
Aman <aakashaman1770@gmail.com>
2020-11-09 16:52:48 +0530

    Added files

---
+++ Strings/string_Compression.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class string_Compression {
+
+	public static String compression1(String str){
+		String s = str.charAt(0) + ""; // To convert the string into a character
+
+		for(int i=1; i < str.length(); i++){
+
+		    char curr = str.charAt(i);
+		    char prev = str.charAt(i-1);
+
+		    if(curr != prev){
+		        s += curr;
+		    }
+		}
+		return s;
+	}
+
+	public static String compression2(String str){
+		String s = str.charAt(0) + ""; // To conver the string into a character
+		int count = 1;
+		for(int i=1; i < str.length(); i++){
+
+		    char curr = str.charAt(i);
+		    char prev = str.charAt(i-1);
+
+		    if(curr == prev){
+		        count++;
+		    }
+		    else{
+		        if(count > 1){
+		            s += count;
+		            count = 1;
+		        }
+		        s += curr;
+		    }
+
+		}
+	    if( count > 1){
+	        s += count;
+	    }
+	    return s;
+
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(compression1(str));
+		System.out.println(compression2(str));
+	}
+
+}
--- demo.java
+++ demo.java
@@ -1,14 +1,46 @@
+import java.io.*;
 import java.util.*;
 
-public class demo{
-  public static void main(String[] args) {
+public class demo {
+      public static boolean isPalindrome(String subString){
 
-    String str = new String("abcde");
+    int i= 0;
+    int j = subString.length() -1;
 
-    for(int i=0; i < str.length(); i++){
-      for(int j= i+1; j <= str.length(); j++){
-        System.out.println(str.substring(i,j));
+    while( i <= j){
+      char ch1 = subString.charAt(i);
+      char ch2 = subString.charAt(j);
+
+      if(ch1 != ch2){
+        return false;
+      }
+      else{
+        i++;
+        j--;
       }
     }
+    return true;
   }
+
+	public static void solution(String str){
+		//write your code here
+				  //Getting all the printSubstring
+    for(int i=0; i < str.length(); i++){
+      for(int j= i+ 1; j <= str.length(); j++){
+        String subString  = str.substring(i,j);
+        if(isPalindrome(subString) == true){
+          System.out.println(subString);
+        }
+      }
+    }
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		solution(str);
+
+
+	}
+
 }
