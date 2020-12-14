019b97a752b72bc61a10426eb1a4d9d1e19c032d
Aman <aakashaman1770@gmail.com>
2020-12-14 14:39:11 +0530

    Added Files

---
+++ Generic Tree/Size_of_tree.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Size_of_tree {
+
+    private static class Node {
+        int data; // Data of the Node
+        ArrayList<Node> children = new ArrayList<>(); // Children array of each node
+
+    }
+
+    public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+
+        for (Node child : node.children) {
+            display(child);
+        }
+    }
+
+    // This function will return the size of the generic tree
+    public static int size(Node node) {
+        int s = 0;
+
+        for (Node child : node.children) {
+            s = s + size(child);
+        }
+
+        s = s + 1;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
+                -1 };
+
+        Node root = null; // Root Node
+
+        Stack<Node> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+
+                Node temp = new Node();
+                temp.data = arr[i];
+                if (st.size() > 0) {
+                    st.peek().children.add(temp);
+                } else {
+                    root = temp;
+                }
+                st.push(temp);
+            }
+        }
+
+        display(root);
+        int getSize = size(root);
+        System.out.println("\n\nSize of tree is: " + getSize);
+    }
+}
